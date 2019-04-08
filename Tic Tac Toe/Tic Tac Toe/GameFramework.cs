6d7abd498d6fc7c71f44b9ae7012582966c1c95f
Linnear Li <linnearli@outlook.com>
2019-04-08 21:31:02 +0200

    Vorbereitungen für die Implementierung von GameFramework

--- Tic Tac Toe/Tic Tac Toe/GameFramework.cs
+++ Tic Tac Toe/Tic Tac Toe/GameFramework.cs
@@ -4,11 +4,25 @@ using System.Text;
 
 namespace Tic_Tac_Toe.GameFramework
 {
+    /*
+     * Regelt die Interaktion mit den Spielern innerhalb einer Partie.
+     */
     class Game
     {
         public Game()
         {
+            //Vorbereitungen treffen
+        }
 
+        /*
+         * Startet eine einzelne Runde des Spiels und gibt zurück, wenn diese beendet ist.
+         */
+        public void PlayMatch()
+        {
+            Console.WriteLine("RUNDE BEGINNT...");
+            //Runde spielen
+            Console.WriteLine("RUNDE ABGESCHLOSSEN");
+            //Ergebnis ausgeben
         }
     }
 
